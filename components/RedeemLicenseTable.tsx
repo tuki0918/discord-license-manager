d40956a66f2e76ad3e401b72fda4e5ed31e657bc
Y.Yamamoto <tuki0918@gmail.com>
2024-11-26 01:02:34 +0900

    Add License and RedeemLicense tables for detailed license management

---
+++ components/RedeemLicenseTable.tsx
@@ -0,0 +1,78 @@
+import {
+	Table,
+	TableBody,
+	TableCell,
+	TableHead,
+	TableHeader,
+	TableRow,
+} from "@/components/ui/table";
+import type { RedeemLicenseType } from "@/domain/models";
+import { cn } from "@/libs/utils";
+import { format } from "date-fns";
+import { Check, CircleX } from "lucide-react";
+import type { FC } from "react";
+
+const RedeemLicenseTable: FC<{
+	redeemLicenses: RedeemLicenseType[];
+}> = ({ redeemLicenses }) => {
+	return (
+		<div className="rounded-xl border bg-card text-card-foreground shadow p-2">
+			<Table>
+				<TableHeader>
+					<TableRow>
+						<TableHead className="text-center">ID</TableHead>
+						<TableHead className="text-center">CODE</TableHead>
+						<TableHead>REDEEMED_AT</TableHead>
+						<TableHead>DISCORD_ID</TableHead>
+						<TableHead>ROLE_ID</TableHead>
+						<TableHead className="text-center">ALIVE</TableHead>
+					</TableRow>
+				</TableHeader>
+				<TableBody>
+					{redeemLicenses.length > 0 ? (
+						redeemLicenses.map((redeemLicense) => (
+							<TableRow
+								key={redeemLicense.id}
+								className={cn({
+									"bg-muted": redeemLicense.status === "disabled",
+								})}
+							>
+								<TableCell className="text-center">
+									{redeemLicense.id}
+								</TableCell>
+								<TableCell className="text-center">
+									{redeemLicense.code}
+								</TableCell>
+								<TableCell>
+									{format(redeemLicense.redeemed_at, "yyyy/MM/dd HH:mm:ss")}
+								</TableCell>
+								<TableCell>{redeemLicense.discord_id}</TableCell>
+								<TableCell>{redeemLicense.discord_grant_role_id}</TableCell>
+								<TableCell className="text-center">
+									<span className="sr-only">
+										{redeemLicense.status === "enable" ? "Enabled" : "Disabled"}
+									</span>
+									<span className="flex items-center justify-center">
+										{redeemLicense.status === "enable" ? (
+											<Check className="text-green-600" />
+										) : (
+											<CircleX className="text-gray-400" />
+										)}
+									</span>
+								</TableCell>
+							</TableRow>
+						))
+					) : (
+						<TableRow>
+							<TableCell colSpan={6} className="text-center">
+								No results.
+							</TableCell>
+						</TableRow>
+					)}
+				</TableBody>
+			</Table>
+		</div>
+	);
+};
+
+export default RedeemLicenseTable;
