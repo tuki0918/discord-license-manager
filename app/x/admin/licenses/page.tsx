6d53e04766366c386f0b7662c10ada55946d0dec
Y.Yamamoto <tuki0918@gmail.com>
2024-11-25 23:27:59 +0900

    Enhance LicenseTable layout and styling with improved data presentation and status indicators

--- app/x/admin/licenses/page.tsx
+++ app/x/admin/licenses/page.tsx
@@ -7,7 +7,15 @@ export default async function Page() {
 	return (
 		<div className="container mx-auto">
 			<div className="h-screen flex items-center justify-center">
-				<LicenseTable licenses={licenses} />
+				<div>
+					<div className="mb-4">
+						<h2 className="text-3xl font-bold">License</h2>
+						<p className="text-muted-foreground">Manage your licenses here.</p>
+					</div>
+					<div>
+						<LicenseTable licenses={licenses} />
+					</div>
+				</div>
 			</div>
 		</div>
 	);
--- components/LicenseTable.tsx
+++ components/LicenseTable.tsx
@@ -8,48 +8,71 @@ import {
 	TableRow,
 } from "@/components/ui/table";
 import type { LicenseType } from "@/domain/models";
+import { cn } from "@/libs/utils";
+import { format } from "date-fns";
+import { Check, CircleX } from "lucide-react";
 import type { FC } from "react";
 
 const LicenseTable: FC<{
 	licenses: LicenseType[];
 }> = ({ licenses }) => {
 	return (
-		<Table>
-			<TableHeader>
-				<TableRow>
-					<TableHead>ID</TableHead>
-					<TableHead>CODE</TableHead>
-					<TableHead>NAME</TableHead>
-					<TableHead>STATUS</TableHead>
-					<TableHead>ROLE_ID</TableHead>
-					<TableHead>EXPIRED_AT</TableHead>
-					<TableHead />
-				</TableRow>
-			</TableHeader>
-			<TableBody>
-				{licenses.length > 0 ? (
-					licenses.map((license) => (
-						<TableRow key={license.id}>
-							<TableCell>{license.id}</TableCell>
-							<TableCell>{license.code}</TableCell>
-							<TableCell>{license.name}</TableCell>
-							<TableCell>{license.status}</TableCell>
-							<TableCell>{license.discord_grant_role_id}</TableCell>
-							<TableCell>{license.expired_at.toDateString()}</TableCell>
-							<TableCell>
-								<LicenseTableRowAction license={license} />
+		<div className="rounded-xl border bg-card text-card-foreground shadow p-2">
+			<Table>
+				<TableHeader>
+					<TableRow>
+						<TableHead>ID</TableHead>
+						<TableHead>CODE</TableHead>
+						<TableHead>NAME</TableHead>
+						<TableHead>STATUS</TableHead>
+						<TableHead>ROLE_ID</TableHead>
+						<TableHead>EXPIRED_AT</TableHead>
+						<TableHead />
+					</TableRow>
+				</TableHeader>
+				<TableBody>
+					{licenses.length > 0 ? (
+						licenses.map((license) => (
+							<TableRow
+								key={license.id}
+								className={cn({
+									"bg-muted": license.status === "disabled",
+								})}
+							>
+								<TableCell className="text-center">{license.id}</TableCell>
+								<TableCell>{license.code}</TableCell>
+								<TableCell>{license.name}</TableCell>
+								<TableCell className="text-center">
+									<span className="sr-only">
+										{license.status === "enable" ? "Enabled" : "Disabled"}
+									</span>
+									<span className="flex items-center justify-center">
+										{license.status === "enable" ? (
+											<Check className="text-green-600" />
+										) : (
+											<CircleX className="text-gray-400" />
+										)}
+									</span>
+								</TableCell>
+								<TableCell>{license.discord_grant_role_id}</TableCell>
+								<TableCell>
+									{format(license.expired_at, "yyyy/MM/dd HH:mm")}
+								</TableCell>
+								<TableCell>
+									<LicenseTableRowAction license={license} />
+								</TableCell>
+							</TableRow>
+						))
+					) : (
+						<TableRow>
+							<TableCell colSpan={7} className="text-center">
+								No results.
 							</TableCell>
 						</TableRow>
-					))
-				) : (
-					<TableRow>
-						<TableCell colSpan={7} className="text-center">
-							No results.
-						</TableCell>
-					</TableRow>
-				)}
-			</TableBody>
-		</Table>
+					)}
+				</TableBody>
+			</Table>
+		</div>
 	);
 };
 
