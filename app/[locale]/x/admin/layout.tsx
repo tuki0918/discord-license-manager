b4ad3e70d1f99377e575d03a07057a2e7a82d7ae
Y.Yamamoto <tuki0918@gmail.com>
2024-11-29 15:44:04 +0900

    Add admin access check in PageLayout component

--- app/[locale]/x/admin/layout.tsx
+++ app/[locale]/x/admin/layout.tsx
@@ -1,10 +1,17 @@
 import SideBar from "@/components/layouts/SideBar";
+import { auth } from "@/utils/auth";
+import { env } from "@/utils/dotenv";
+import { redirect } from "next/navigation";
 
-export default function PageLayout({
+export default async function PageLayout({
 	children,
 }: Readonly<{
 	children: React.ReactNode;
 }>) {
+	const session = await auth();
+	// If the user is not an admin, redirect to the homepage
+	if (session?.user?.email !== env.ADMIN_EMAIL) redirect("/");
+
 	return (
 		<>
 			<div className="grid grid-cols-5">
