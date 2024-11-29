4086247e9c17a07c0e7303464dfaf2eb77c0099c
Y.Yamamoto <tuki0918@gmail.com>
2024-11-29 11:15:52 +0900

    Add NEXTAUTH_URL to environment configuration and validation schema

--- utils/dotenv.ts
+++ utils/dotenv.ts
@@ -2,6 +2,7 @@ import { g, validateEnv } from "@/libs/dotenv";
 
 const schema = g.envObject({
 	DATABASE_URL: g.envString(),
+	NEXTAUTH_URL: g.envString().url(),
 	AUTH_SECRET: g.envString(),
 });
 
