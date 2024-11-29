427f362fbfedc45dcda27507fd22666920341485
Y.Yamamoto <tuki0918@gmail.com>
2024-11-29 10:55:28 +0900

    Add AUTH_SECRET to environment configuration and example

--- utils/dotenv.ts
+++ utils/dotenv.ts
@@ -2,6 +2,7 @@ import { g, validateEnv } from "@/libs/dotenv";
 
 const schema = g.envObject({
 	DATABASE_URL: g.envString(),
+	AUTH_SECRET: g.envString(),
 });
 
 export const env = validateEnv(schema);
