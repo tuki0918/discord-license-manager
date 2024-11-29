45e92c0c8134c74c35f4adf19846e5afcd83886d
Y.Yamamoto <tuki0918@gmail.com>
2024-11-29 11:46:42 +0900

    Rename NEXTAUTH_URL to AUTH_URL in environment configuration and validation schema

--- utils/dotenv.ts
+++ utils/dotenv.ts
@@ -2,7 +2,7 @@ import { g, validateEnv } from "@/libs/dotenv";
 
 const schema = g.envObject({
 	DATABASE_URL: g.envString(),
-	NEXTAUTH_URL: g.envString().url(),
+	AUTH_URL: g.envString().url(),
 	AUTH_SECRET: g.envString(),
 	AUTH_DISCORD_ID: g.envString(),
 	AUTH_DISCORD_SECRET: g.envString(),
