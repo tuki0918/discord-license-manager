ed49c6b3a3466df8ad093c0ce0a41589277977d8
Y.Yamamoto <tuki0918@gmail.com>
2024-12-01 16:39:43 +0900

    Add user authentication and dropdown menu to the main page

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,7 +1,23 @@
-export default function Page() {
+import SignInButton from "@/components/SignInButton";
+import UserDropdownMenu from "@/components/UserDropdownMenu";
+import { auth } from "@/utils/auth";
+import { env } from "@/utils/dotenv";
+
+export default async function Page() {
+	const session = await auth();
+	const isLoggedIn = !!session?.user;
+	const isAdmin = session?.user?.email === env.ADMIN_EMAIL;
 	return (
-		<div className="h-screen flex items-center justify-center">
-			<h1>Hello World</h1>
+		<div className="relative h-screen flex items-center justify-center">
+			{session?.user && (
+				<div className="absolute top-4 right-4">
+					<UserDropdownMenu user={session.user} isAdmin={isAdmin} />
+				</div>
+			)}
+			<div>
+				<h1>Hello World</h1>
+				{!isLoggedIn && <SignInButton />}
+			</div>
 		</div>
 	);
 }
